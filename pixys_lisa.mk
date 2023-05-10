b0a43a3c81ef975db5836b2f47c56d05ec6c378c
TechWiz007 <gajarebhargav@gmail.com>
2023-05-10 13:57:13 +0530

    lisa: PixysOS

--- pixys_lisa.mk
+++ pixys_lisa.mk
@@ -11,16 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lisa device
 $(call inherit-product, device/xiaomi/lisa/device.mk)
 
-# Inherit some common PixelOS stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common PixysOS stuff
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
+TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lisa
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_lisa
+PRODUCT_NAME := pixys_lisa
 PRODUCT_MODEL := Xiaomi 11 Lite NE
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
